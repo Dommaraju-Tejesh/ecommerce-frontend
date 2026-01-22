5d054a2d50d43b03bf6c979ab0dfcc4888127cf8
Tejesh <d.tejeshraju67@gmail.com>
2026-01-22 11:07:00 +0530

    use env api base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-backend-ip7e.onrender.com/api/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default api;
