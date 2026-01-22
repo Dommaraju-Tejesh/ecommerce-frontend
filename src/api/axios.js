e965a87a618b982228281ad97ede4f480780614b
Tejesh <d.tejeshraju67@gmail.com>
2026-01-22 10:58:36 +0530

    fix api base url for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-1p5i.onrender.com/api/",
+  baseURL: "https://ecommerce-backend-ip7e.onrender.com/api/",
 });
 
 export default api;
