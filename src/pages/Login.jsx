9d82f65c8de5cf29a493e37540196004f94721df
Tejesh <d.tejeshraju67@gmail.com>
2026-01-23 21:46:07 +0530

    signup and login connected

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await api.post("/api/token/", {
+        username: email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.access);
+      alert("Login successful!");
+      navigate("/");
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      /><br /><br />
+
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignup = async () => {
+    try {
+      await api.post("/api/users/register/", {
+        email,
+        password,
+      });
+      alert("User registered! Please login.");
+      navigate("/login");
+    } catch (err) {
+      alert("Signup failed");
+    }
+  };
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Register Here</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      /><br /><br />
+
+      <button onClick={handleSignup}>Signup</button>
     </div>
   );
 }
