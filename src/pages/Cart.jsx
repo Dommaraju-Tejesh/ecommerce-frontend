067f0e15fea57a779fc5100c758ff04d0247d79b
Tejesh <d.tejeshraju67@gmail.com>
2026-01-23 21:41:52 +0530

    show cart items

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,30 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
 export default function Cart() {
+  const { cart, removeFromCart } = useContext(CartContext);
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>CART ITEMS</h1>
+
+      {cart.length === 0 && <p>Your cart is empty</p>}
+
+      {cart.map((item) => (
+        <div key={item.id} style={card}>
+          <h3>{item.name}</h3>
+          <p>Rs {item.price}</p>
+          <button onClick={() => removeFromCart(item.id)}>
+            Remove
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
+
+const card = {
+  border: "1px solid #ccc",
+  padding: "10px",
+  margin: "10px 0",
+};
